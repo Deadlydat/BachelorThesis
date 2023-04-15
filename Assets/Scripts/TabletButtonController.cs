424149569d902fa9f0c4d65dd19f391d8d61d179
Deadlydat <81553941+Deadlydat@users.noreply.github.com>
2023-04-15 12:33:17 +0200

    environment nearly done

---
+++ Assets/Scripts/TabletButtonController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabletButtonController : MonoBehaviour
+{
+    public void TaskOnClickA()
+    {
+        Debug.Log("You have clicked the button a!");
+    }
+    public void TaskOnClickB()
+    {
+        Debug.Log("You have clicked the button b!");
+    }
+    public void TaskOnClickC()
+    {
+        Debug.Log("You have clicked the button c!");
+    }
+    public void TaskOnClickD()
+    {
+        Debug.Log("You have clicked the button d!");
+
+        
+    }
+}
