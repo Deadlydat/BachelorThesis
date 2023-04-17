71b9a49a46f46063cb645be991c97c99b795419d
Deadlydat <81553941+Deadlydat@users.noreply.github.com>
2023-04-17 13:59:22 +0200

    ui ablauf läuft

---
+++ Assets/Scripts/Questions.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Questions : MonoBehaviour
+{
+
+    public TextAsset questionsJSON;
+    [System.Serializable]
+    public class Question
+    {
+        public string question;
+
+        public string answerA;
+        public bool boolA;
+
+        public string answerB;
+        public bool boolB;
+
+
+        public string answerC;
+        public bool boolC;
+
+
+        public string answerD;
+        public bool boolD;
+
+    }
+
+    [System.Serializable]
+    public class QuestionList
+    {
+        public Question[] questions;
+
+    }
+
+
+
+    public QuestionList mQuestionList = new QuestionList();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mQuestionList = JsonUtility.FromJson<QuestionList>(questionsJSON.text);
+
+
+    }
+
+}
--- Assets/Scripts/TabletButtonController.cs
+++ Assets/Scripts/TabletButtonController.cs
@@ -1,25 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using static Questions;
 
 public class TabletButtonController : MonoBehaviour
 {
+    private Text Header;
+    private Text TextButtonA;
+    private Text TextButtonB;
+    private Text TextButtonC;
+    private Text TextButtonD;
+
+    public Questions questionsScript;
+    public GameObject startObject;
+    public GameObject uiTablet;
+
+    private QuestionList questionsList;
+    private int questionNumber = 0;
+
+
+    void Start()
+    {
+        Header = GameObject.Find("HeaderUi").GetComponentInChildren<Text>();
+        TextButtonA = GameObject.Find("TextButtonA").GetComponentInChildren<Text>();
+        TextButtonB = GameObject.Find("TextButtonB").GetComponentInChildren<Text>();
+        TextButtonC = GameObject.Find("TextButtonC").GetComponentInChildren<Text>();
+        TextButtonD = GameObject.Find("TextButtonD").GetComponentInChildren<Text>();
+
+        questionsList = questionsScript.mQuestionList;
+        SetQuestion(questionNumber);
+        questionNumber++;
+    }
+
+
+
+    void OnEnable()
+    {
+        //darf das aller erstemal nicht gecalled werden
+        SetQuestion(questionNumber);
+        questionNumber++;
+    }
+
+
+
+    private void SetQuestion(int questionNumber)
+    {
+
+        Header.text = questionNumber + 1 + "/" + questionsList.questions.Length + " :  " +
+            questionsList.questions[questionNumber].question;
+
+        TextButtonA.text = questionsList.questions[questionNumber].answerA;
+        TextButtonB.text = questionsList.questions[questionNumber].answerB;
+        TextButtonC.text = questionsList.questions[questionNumber].answerC;
+        TextButtonD.text = questionsList.questions[questionNumber].answerD;
+
+    }
+
+    private void SetNewQuestion()
+    {
+        if (questionNumber < questionsList.questions.Length)
+        {
+            SetQuestion(questionNumber);
+            questionNumber++;
+        }
+        else
+        {
+            questionNumber = 0;
+            startObject.SetActive(true);
+
+            uiTablet.SetActive(false);
+        }
+
+    }
+
+
     public void TaskOnClickA()
     {
         Debug.Log("You have clicked the button a!");
+        SetNewQuestion();
+
     }
     public void TaskOnClickB()
     {
         Debug.Log("You have clicked the button b!");
+        SetNewQuestion();
     }
     public void TaskOnClickC()
     {
         Debug.Log("You have clicked the button c!");
+        SetNewQuestion();
     }
     public void TaskOnClickD()
     {
         Debug.Log("You have clicked the button d!");
+        SetNewQuestion();
 
-        
+
     }
 }
